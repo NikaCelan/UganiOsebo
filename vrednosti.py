243fb3f84bddf689cd5d6b0f8c26e465c8bab5d8
Nika Celan <nika.celan8@gmail.com>
2020-08-03 15:36:15 +0200

    začasna datoteka

---
+++ vrednosti.py
@@ -0,0 +1,23 @@
+            % if kriterij == spol:
+            <option>moški </option>
+            <option>ženska </option>
+            % elif kriterij == barva_las:
+            <option>blond </option>
+            <option>črna </option>
+            <option>rdeča </option>
+            <option>rjava </option>
+            <option>lasje niso vidni </option>
+            % elif kriterij == dolžina_las:
+            <option> </option>
+            <option> </option>
+            [kratki, dolgi]
+            % elif kriterij == barva_majice:
+            <option>rdeča </option>
+            <option>črna </option>
+            <option>siva </option>
+            <option>zelena </option>
+            <option>bela </option>
+            <option>modra </option>
+            % elif kriterij == usta:
+            <option>odprta </option>
+            <option>zaprta </option>
